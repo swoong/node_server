2ac9804418e1faed5f18a179e0b8b4e130cf080a
swoong <swoong69@gmail.com>
2023-03-20 12:53:56 +0900

    first commit

---
+++ config/database.js
@@ -0,0 +1,18 @@
+module.exports = {
+    host     : 'svc.sel3.cloudtype.app',
+    port     : '30855',
+    user     : 'root',
+    password : 'insa0000',
+    database : 'my_db'
+  };
+
+/*
+module.exports = {
+    host     : '3.38.108.245',
+    port     : '51988',
+    user     : 'root',
+    password : 'insa0000',
+    database : 'my_db'
+  };
+*/
+  
